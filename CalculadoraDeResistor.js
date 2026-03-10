454f5f3ac7a654801feac386cd92ea94da697384
Matheus Lacerda <matheuslacerda.mala@gmail.com>
2026-03-10 20:16:38 -0300

    Adicionando conversão de Ohm para quilo Ohm e para mega Ohm

--- CalculadoraDeResistor.js
+++ CalculadoraDeResistor.js
@@ -15,7 +15,8 @@ function CalcularResistencia(){
         10 : 5,
         11 : 10
     },
-    resultado = document.getElementById("resultado")
+    resultado = document.getElementById("resultado"),
+    unidadeDeMedida = "";
     console.log(faixas);
     if(qtdFaixas != 4 && qtdFaixas != 5){
         console.log("Quantidade de faixas inválida!");
@@ -29,15 +30,22 @@ function CalcularResistencia(){
             tolerancia = "X"
         }
     } else {
-        numResistencia = Number("" + faixas[0] + "" + faixas[1] + "" + faixas[2]) * cores[faixas[3]];
-        if(Number.isInteger(tolerancias[faixas[4]])){
+        numResistencia = parseInt("" + faixas[0] + "" + faixas[1] + "" + faixas[2]) * cores[faixas[3]];
+        if(parseInt.isInteger(tolerancias[faixas[4]])){
             tolerancia = tolerancias[faixas[4]];
         } else {
             tolerancia = "X"
         }
     }
+    if(String(numResistencia).length >= 7){
+        numResistencia /= 1000000
+        unidadeDeMedida = "M";
+    } else if(String(numResistencia).length >= 4){
+        numResistencia /= 1000
+        unidadeDeMedida = "k";
+    }
     resultado.style.width = '380px';
-    resultado.innerHTML = "Resistência: " + numResistencia + "Ω || Tolerância: +/- " + tolerancia + "%";
+    resultado.innerHTML = "Resistência: " + numResistencia + unidadeDeMedida + "Ω || Tolerância: +/- " + tolerancia + "%";
 }
 function QuatroOuCinco(){
     let valorInserido = document.getElementById("qtdFaixas").value, quintaFaixa = document.getElementById("Faixa5");
