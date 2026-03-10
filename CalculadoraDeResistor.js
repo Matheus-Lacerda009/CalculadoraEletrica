3fa41b55794cf7d10d920c3f10cb74b319927cc8
Matheus Lacerda <matheuslacerda.mala@gmail.com>
2026-03-10 20:08:32 -0300

    Foi criada uma interface para a calculadora de resistores

--- CalculadoraDeResistor.js
+++ CalculadoraDeResistor.js
@@ -7,7 +7,7 @@ function CalcularResistencia(){
         parseInt(document.getElementById("Faixa3").value),
         parseInt(document.getElementById("Faixa4").value),
         parseInt(document.getElementById("Faixa5").value)],
-    cores = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000],
+    cores = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000],
     tolerancias = {
         1 : 1,
         2 : 2,
@@ -15,18 +15,35 @@ function CalcularResistencia(){
         10 : 5,
         11 : 10
     },
-    resultado = document.getElementById("Resultado")
+    resultado = document.getElementById("resultado")
     console.log(faixas);
     if(qtdFaixas != 4 && qtdFaixas != 5){
         console.log("Quantidade de faixas inválida!");
         return 0;
     }
     if(qtdFaixas == 4){
-        numResistencia = parseInt("" + faixas[0] + "" + faixas[1]) * cores[faixas[2]];
-        tolerancia = tolerancias[faixas[3]];
+        numResistencia = Number("" + faixas[0] + "" + faixas[1]) * cores[faixas[2]];
+        if(Number.isInteger(tolerancias[faixas[3]])){
+            tolerancia = tolerancias[faixas[3]];
+        } else {
+            tolerancia = "X"
+        }
     } else {
-        numResistencia = parseInt("" + faixas[0] + "" + faixas[1] + "" + faixas[2]) * cores[faixas[3]];
-        tolerancia = tolerancias[faixas[4]];
+        numResistencia = Number("" + faixas[0] + "" + faixas[1] + "" + faixas[2]) * cores[faixas[3]];
+        if(Number.isInteger(tolerancias[faixas[4]])){
+            tolerancia = tolerancias[faixas[4]];
+        } else {
+            tolerancia = "X"
+        }
     }
+    resultado.style.width = '380px';
     resultado.innerHTML = "Resistência: " + numResistencia + "Ω || Tolerância: +/- " + tolerancia + "%";
+}
+function QuatroOuCinco(){
+    let valorInserido = document.getElementById("qtdFaixas").value, quintaFaixa = document.getElementById("Faixa5");
+    if(valorInserido <= 4){
+        quintaFaixa.disabled = true;
+    } else {
+        quintaFaixa.disabled = false;
+    }
 }
