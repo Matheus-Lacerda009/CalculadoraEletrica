094897ec9ac3c48eaf83993123b13ea9a1fe58c7
Matheus Lacerda <matheuslacerda.mala@gmail.com>
2026-03-09 21:20:47 -0300

    Calculadora de resistor foi terminada, juntamente de uma interface básica com HTML

--- CalculadoraDeResistor.js
+++ CalculadoraDeResistor.js
@@ -1,11 +1,40 @@
-/*Inputs - Se são 4 ou 5 faixas;
-Primeira faixa, segunda faixa, terceira faixa, quarta faixa e (dependendo) quinta faixa*/
-let qtdFaixas = parseint(prompt("Insira a quantidade de faixas:")), faixas = [], i;
-for(i = 0; i < qtdFaixas; i++){
-    faixas.push(parseInt(prompt("Insira a cor da faixa:\n1 - Preto\n2 - Marrom\n3 - Vermelho\n4 - Laranja\n5 - Amarelo\n6 - Verde\n7 - Azul\n8 - Violeta\n9 - Cinza\n10 - Branco\n11 - Ouro\n12 - Prata\n13 - Branco:")));
-    if(faixas[i] > 13 || faixas[i] < 1){
-        alert("ERRO! Valor fora do esperado");
-        i--;
-        faixas.pop();
+function CalcularResistencia(){
+    let tolerancia, numResistencia,
+    qtdFaixas = document.getElementById("qtdFaixas").value,
+    faixas = [
+        parseInt(document.getElementById("Faixa1").value),
+        parseInt(document.getElementById("Faixa2").value),
+        parseInt(document.getElementById("Faixa3").value),
+        parseInt(document.getElementById("Faixa4").value),
+        parseInt(document.getElementById("Faixa5").value)],
+    cores = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000],
+    tolerancias = {
+        1 : 1,
+        2 : 2,
+        9 : 20,
+        10 : 5,
+        11 : 10
+    },
+    resultado = document.getElementById("Resultado")
+    console.log(faixas);
+    if(qtdFaixas != 4 && qtdFaixas != 5){
+        console.log("Quantidade de faixas inválida!");
+        return 0;
+    }
+    if(qtdFaixas == 4){
+        numResistencia = parseInt("" + faixas[0] + "" + faixas[1]) * cores[faixas[2]];
+        tolerancia = tolerancias[faixas[3]];
+    } else {
+        numResistencia = parseInt("" + faixas[0] + "" + faixas[1] + "" + faixas[2]) * cores[faixas[3]];
+        tolerancia = tolerancias[faixas[4]];
+    }
+    resultado.innerHTML = "Resistência: " + numResistencia + "Ω || Tolerância: +/- " + tolerancia + "%";
+}
+function QuatroOuCindo(){
+    let valorInserido = document.getElementById("qtdFaixas").value, quintaFaixa = document.getElementById("Faixa5"), faixas = document.getElementById("containerPai"), salvarQuintaFaixa;
+    if(valorInserido <= 4){
+        quintaFaixa.style.display = 'none'
+    } else {
+        quintaFaixa.style.display = 'flex'
     }
 }
