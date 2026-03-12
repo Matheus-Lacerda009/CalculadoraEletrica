8a6181ec9d766ac4b865ed90bc80a004844f4f44
Matheus Lacerda <matheuslacerda.mala@gmail.com>
2026-03-12 20:49:48 -0300

    Aprimoração visual da interface e correção de bugs em relação a seleção de cores. O menu foi atualizado, alterando o formato dos selects e foi adicionado uma ilustração de um resistor. A cor das faixas de tal resistor se alteram conforme a seleção das cores nos selects

--- Resitencia/CalculadoraDeResistor.js
+++ Resitencia/CalculadoraDeResistor.js
@@ -7,7 +7,7 @@ function CalcularResistencia(){
         parseInt(document.getElementById("Faixa3").value),
         parseInt(document.getElementById("Faixa4").value),
         parseInt(document.getElementById("Faixa5").value)],
-    cores = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000],
+    cores = [1, 10, 10 ** 2, 10 ** 3, 10 ** 4, 10 ** 5, 10 ** 6, 10 ** 7, 10 ** 8, 10 ** 9],
     tolerancias = {
         1 : 1,
         2 : 2,
@@ -17,7 +17,6 @@ function CalcularResistencia(){
     },
     resultado = document.getElementById("resultado"),
     unidadeDeMedida = "";
-    console.log(faixas);
     if(qtdFaixas != 4 && qtdFaixas != 5){
         console.log("Quantidade de faixas inválida!");
         return 0;
@@ -31,7 +30,7 @@ function CalcularResistencia(){
         }
     } else {
         numResistencia = parseInt("" + faixas[0] + "" + faixas[1] + "" + faixas[2]) * cores[faixas[3]];
-        if(parseInt.isInteger(tolerancias[faixas[4]])){
+        if(Number.isInteger(tolerancias[faixas[4]])){
             tolerancia = tolerancias[faixas[4]];
         } else {
             tolerancia = "X"
@@ -44,14 +43,44 @@ function CalcularResistencia(){
         numResistencia /= 1000
         unidadeDeMedida = "k";
     }
-    resultado.style.width = '380px';
     resultado.innerHTML = "Resistência: " + numResistencia + unidadeDeMedida + "Ω || Tolerância: +/- " + tolerancia + "%";
 }
 function QuatroOuCinco(){
-    let valorInserido = document.getElementById("qtdFaixas").value, quintaFaixa = document.getElementById("Faixa5");
+    let valorInserido = document.getElementById("qtdFaixas").value, quintaFaixa = document.getElementById("Faixa5"), quartaFaixaIlustracao = document.getElementById("faixa4cor"), faixasDiv = document.getElementById("faixasCores");
     if(valorInserido <= 4){
         quintaFaixa.disabled = true;
+        faixa4cor.style.display = "none";
+        faixasDiv.style.right = "10px";
     } else {
         quintaFaixa.disabled = false;
+        faixa4cor.style.display = "block";
+        faixasDiv.style.right = "20px";
+    }
+}
+function trocaCor(){
+    let faixas = [
+        parseInt(document.getElementById("Faixa1").value),
+        parseInt(document.getElementById("Faixa2").value),
+        parseInt(document.getElementById("Faixa3").value),
+        parseInt(document.getElementById("Faixa4").value),
+        parseInt(document.getElementById("Faixa5").value)],
+    corFaixasFront = ["black", "rgb(80, 40, 0)", "red", "rgb(240, 120, 0)", "yellow", "green", "blue", "rgb(150, 0, 200)", "gray", "white", "rgb(150, 110, 0)", "silver"],
+    faixa1 = document.getElementById("faixa1cor"),
+    faixa2 = document.getElementById("faixa2cor"),
+    faixa3 = document.getElementById("faixa3cor"),
+    faixa4 = document.getElementById("faixa4cor"),
+    faixa5 = document.getElementById("faixaToleranciaCor"),
+    valorInserido = document.getElementById("qtdFaixas").value;
+    if(valorInserido <= 4){
+        faixa1.style.backgroundColor = corFaixasFront[faixas[0]];
+        faixa2.style.backgroundColor = corFaixasFront[faixas[1]];
+        faixa3.style.backgroundColor = corFaixasFront[faixas[2]];
+        faixa5.style.backgroundColor = corFaixasFront[faixas[3]];
+    } else {
+        faixa1.style.backgroundColor = corFaixasFront[faixas[0]];
+        faixa2.style.backgroundColor = corFaixasFront[faixas[1]];
+        faixa3.style.backgroundColor = corFaixasFront[faixas[2]];
+        faixa4.style.backgroundColor = corFaixasFront[faixas[3]];
+        faixa5.style.backgroundColor = corFaixasFront[faixas[4]];
     }
 }
